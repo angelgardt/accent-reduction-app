3a811bb3c0e958e994c3de40418c87c462256369
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-04-17 17:02:33 +0300

    change files location, dtw not woking :(

--- core/test/script.R
+++ core/test/script.R
@@ -1,23 +1,35 @@
+# pkgs <- c("tidyverse", "warbleR")
+# install.packages(pkgs)
+
 library(tidyverse)
 library(warbleR)
 
-tibble(sound.files = dir("stims"),
+getwd()
+
+# dir("../data/recs/v1/subj1/homographs/")
+subjs <- dir("../data/recs/v1/")
+subjs |> 
+  map(\(x) sprintf("../data/recs/v1/%s/homographs/", x)) |> 
+  unlist() -> sound_paths_homographs
+sound_paths_homographs |> 
+  map(dir, full.names=TRUE) |> 
+  unlist() -> sound_files_homographs
+
+tibble(sound.files = sound_files_homographs,
        selec = 1,
        start = 0,
-       end = duration_wavs(paste0("stims/", 
-                                  dir("stims")))$duration) -> st
+       end = duration_wavs(sound_files_homographs)$duration) -> homographs
 
-info_wavs(paste0("stims/", 
-                 dir("stims")))
+info_wavs(sound_files_homographs)
 
-View(select_table)
+View(homographs)
 warbleR_options(flim = c(1, 10), wl = 200, ovlp = 90, bp = c(0, 5))
 
-sp <- specan(st)
+sp <- specan(homographs)
 sp
 
-xcor <- xcorr(X = st)
+xcor <- xcorr(X = homographs)
 xcor
 
-dtw <- df_DTW(st)
-dtw %>% View
+dtw <- df_DTW(homographs)
+dtw
