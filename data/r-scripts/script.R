9472309d53b665a8c3b980b9eaf780752385a02e
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-05-17 20:03:04 +0300

    up to 24th rec subj1 annotated

--- data/r-scripts/script.R
+++ data/r-scripts/script.R
@@ -12,19 +12,28 @@ str(features)
 
 features |>
   filter(stress %in% 1:3) |>
-  ggplot(aes(f2, f1,
-             label = phoneme, color = stress)) +
-  geom_text() +
-  stat_ellipse() +
-  scale_x_reverse(position = "top") +
-  scale_y_reverse(position="right") +
-  theme(legend.position = "bottom")
+  mutate(stress = factor(stress, ordered = TRUE, levels = 1:3))-> vowels
 
 
-features |>
-  filter(stress %in% 1:3) |>
-  mutate(stress = as_factor(stress)) |>
+# features |> filter(phoneme == "əᶦ")
+
+vowels |>
   ggplot(aes(stress, duration)) +
   stat_summary(fun.data = mean_cl_boot, geom = "pointrange")
 
+vowels |>
+  aggregate(cbind(f1, f2) ~ phoneme, median) |>
+  as_tibble() |>
+  right_join(vowels |> select(phoneme, stress)) -> centroids
+
+vowels |>
+  ggplot(aes(f2, f1,
+             label = phoneme, color = phoneme)) +
+  geom_text() +
+  stat_ellipse() +
+  geom_point(data = centroids, aes(f2, f1, color = phoneme), size = 2) +
+  scale_x_reverse(position = "top") +
+  scale_y_reverse(position="right") +
+  facet_wrap(~ stress) +
+  guides(color = "none")
 
