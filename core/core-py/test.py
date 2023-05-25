729b745a8a257dd88531f8f33c201959e1a01143
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-05-26 00:44:47 +0300

    testing

--- core/core-py/test.py
+++ core/core-py/test.py
@@ -4,15 +4,20 @@ import librosa
 import numpy as np
 import csv
 
-base_folder = "/home/angelgardt/accent-reduction-app/data/recs/v1/subj1/homographs_audio"
-#file = "24-1.wav"
-# file = "24-2.wav"
+# for linux
+# base_folder = "/home/angelgardt/accent-reduction-app/data/recs/v1/subj1/homographs_audio"
+
+# for mac
+base_folder = "/Users/antonangelgardt/accent-reduction-app/data/recs/v1/subj3/homographs_audio"
+
+subj = re.findall("subj\d+", base_folder)[0]
 files = os.listdir(base_folder)
 
 
 frame = 0.005
-crop = 27
-intensity_threshold = -20
+low_crop = 1
+high_crop = 15
+intensity_threshold = -30
 low_freq = 200
 high_freq = 1000
 gap = 3
@@ -21,22 +26,25 @@ min_duration = 0.02
 results = []
 
 for file in files:
-    suffix = file.split(".")[0]
+    suffix = subj + "_" + file.split(".")[0]
     r = core_stressed_vowel(file,
                             base_folder,
                             frame,
-                            crop,
+                            low_crop,
+                            high_crop,
                             intensity_threshold,
                             low_freq,
                             high_freq,
                             gap,
                             min_duration,
-                            suffix="_"+suffix,
+                            suffix="_" + suffix,
                             sr=None,
                             mono=None,
                             save=True)
     results.append([suffix, r])
 
-with open("results.csv", "w", newline="") as f:
+print(results)
+
+with open(subj + "_results.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(results)
